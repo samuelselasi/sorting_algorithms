821099cd2812b5f826060dbe1e512267f71a17ef
samuelselasi <samelsekasi@gmail.com>
2023-04-21 22:35:47 +0000

    Update header file with bubble sort prototype

--- sort.h
+++ sort.h
@@ -19,4 +19,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* Prototypes for Bubble Sort */
+void bubble_sort(int *array, size_t size);
+
 #endif
